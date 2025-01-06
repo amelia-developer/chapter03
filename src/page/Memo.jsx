a151b78330701daed8851bf98575914bb78ac96b
DESKTOP-SE2AMM7\6736l <1022lys6736@gmail.com>
2025-01-06 22:49:55 +0900

    import 오타 수정

--- src/page/Memo.jsx
+++ src/page/Memo.jsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchClickDateNumber, fetchModifyMemo, insertDateNumber, fetchDeleteMemo, fetchAllMemoList } from '../redux/joinAction'
 
